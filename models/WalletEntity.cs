402dd20a2a0694ed6c2a0355893006f21a3b94fe
teuzowebdeveloper9 <mateussoftwaredeveloper@gmail.com>
2026-01-11 17:14:49 -0300

    feat: add .gitignore and update WalletEntity model with new properties

--- models/WalletEntity.cs
+++ models/WalletEntity.cs
@@ -1,6 +1,14 @@
+using SimplePicPay.Models.Enums;
+
 namespace SimplePicPay.Models;
 
 public class WalletEntity
 {
-    
+    public Guid Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string CPFCNPJ { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public string PasswordHash { get; set; } = string.Empty;
+    public decimal Balance { get; set; }
+    public UserType UserType { get; set; }
 }
